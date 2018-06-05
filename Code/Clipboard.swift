4a8504892e0abe869a8844c54ec13f3de02a6047
Sebastian <contact@flowtoolz.com>
2018-06-05 10:35:49 +0200

    add clipboard that can store copies of objects

---
+++ Code/Clipboard.swift
@@ -0,0 +1,18 @@
+public class Clipboard<Object: Copyable>
+{
+    public init() {}
+    
+    public var count: Int { return storedObjects.count }
+    
+    public func storeCopies(of objects: [Object])
+    {
+        storedObjects = objects.map { $0.copy }
+    }
+    
+    public var copiesOfStoredObjects: [Object]
+    {
+        return storedObjects.map { $0.copy }
+    }
+    
+    private var storedObjects = [Object]()
+}
