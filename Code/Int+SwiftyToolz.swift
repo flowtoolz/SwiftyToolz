cb5b31ccee0b7e5e6af72210c655ad89e5152769
Sebastian <contact@flowtoolz.com>
2018-10-28 20:54:31 +0100

    add typealias Byte and random number generation for UInt8

--- Code/Int+SwiftyToolz.swift
+++ Code/Int+SwiftyToolz.swift
@@ -5,3 +5,13 @@ public extension Int
         return Int(arc4random_uniform(max + 1))
     }
 }
+
+public extension UInt8
+{
+    public static func random(max: UInt8 = .max) -> UInt8
+    {
+        return UInt8(arc4random_uniform(UInt32(max) + 1))
+    }
+}
+
+public typealias Byte = UInt8
