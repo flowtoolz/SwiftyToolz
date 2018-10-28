71090351582aa611f2289c92e373eeedf597c285
Sebastian <contact@flowtoolz.com>
2018-10-28 20:55:22 +0100

    add pure swift based UUID generator

--- Code/String+Flowtoolz.swift
+++ Code/String+Flowtoolz.swift
@@ -16,4 +16,36 @@ public extension String
         
         self = String(utf16CodeUnits: &unicodeCharacter, count: 1)
     }
+    
+    public static var uuid: String
+    {
+        // create random bytes
+        
+        var bytes = [Byte]()
+        
+        for _ in 0 ..< 16 { bytes.append(Byte.random()) }
+        
+        // indicate UUID version and variant
+        
+        bytes[6] = (bytes[6] & 0x0f) | 0x40 // version 4
+        bytes[8] = (bytes[8] & 0x3f) | 0x80 // variant 1
+        
+        // create string representation
+        
+        let ranges = [0 ..< 4, 4 ..< 6, 6 ..< 8, 8 ..< 10, 10 ..< 16]
+        
+        return ranges.map
+        {
+            range in
+            
+            var string = ""
+            
+            for i in range
+            {
+                string += String(bytes[i], radix: 16, uppercase: false)
+            }
+            
+            return string
+        }.joined(separator: "-")
+    }
 }
