d2271472a5f0f81d903aaec5fe035df1ac657903
Sebastian <contact@flowtoolz.com>
2018-11-29 21:26:03 +0100

    Extens Int with a function for repeating a closure n times

--- Code/Int+SwiftyToolz.swift
+++ Code/Int+SwiftyToolz.swift
@@ -1,5 +1,13 @@
 public extension Int
 {
+    func times(_ body: () throws -> Void) rethrows
+    {
+        if self > 0
+        {
+            for _ in 0 ..< self { try body() }
+        }
+    }
+    
     public static func random(max: Int = .max) -> Int
     {
         return Int.random(in: 0 ... max)
