600160af5430cac445cc7b881ee90c157f9e0f10
Sebastian <contact@flowtoolz.com>
2018-12-26 19:48:12 +0100

    Bump version to 1.4.3

--- Package.swift
+++ Package.swift
@@ -1,7 +1,15 @@
+// swift-tools-version:4.2
+
 import PackageDescription
 
-let package = Package(name: "SwiftyToolz",
-                      products: [.library(name: "SwiftyToolz",
-                                          targets: ["SwiftyToolz"])],
-                      targets: [.target(name: "SwiftyToolz",
-                                        dependencies: [])])
+let package = Package(
+    name: "SwiftyToolz",
+    products: [
+        .library(name: "SwiftyToolz",
+                 targets: ["SwiftyToolz"]),
+    ],
+    targets: [
+        .target(name: "SwiftyToolz",
+                path: "Code"),
+    ]
+)
