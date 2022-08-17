fc9d4cd49110dd5d141c4190ef61d0e9b00ecce1
Sebastian Fichtner <contact@flowtoolz.com>
2022-08-17 18:44:26 +0200

    Add TODO

--- Code/BuildEnvironment.swift
+++ Code/BuildEnvironment.swift
@@ -17,6 +17,7 @@ public struct BuildEnvironment
         #endif
     }
     
+    /// TODO: it is still unclear whether this really works as part of an external package. the package seems to receive its own build configuration. just removing "DEBUG" from the project or target build configs does not effect this here. testing it is also difficult, i couldn't run run a macos client locally with release configuration ... so i haven't verified yet that this will return false when compiled with a client app for release ...
     public static var isDebugConfiguration: Bool
     {
         #if DEBUG
