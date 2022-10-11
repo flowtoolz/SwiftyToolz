2e62cb659792f508136f27327d48d06c647fc68b
Sebastian Fichtner <contact@flowtoolz.com>
2022-10-11 11:55:12 +0200

    Make it compile on more platforms

--- Code/Swift+SwiftyToolz/Task+SwiftyToolz.swift
+++ Code/Swift+SwiftyToolz/Task+SwiftyToolz.swift
@@ -1,4 +1,4 @@
-@available(macOS 10.15, *)
+@available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
 public extension Task where Success == Void
 {
     func assumeSuccess() async throws { try await value }
